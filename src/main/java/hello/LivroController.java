5f69e1e1dbac8db78bc5f0791e084642536ebed0
Felipe Silva <felipess@ciandt.com>
2018-06-07 21:42:40 -0300

    Atualizacao

--- src/main/java/hello/LivroController.java
+++ src/main/java/hello/LivroController.java
@@ -1,22 +1,29 @@
 package hello;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class LivroController {
 
-    @RequestMapping(value= "/livro/{id}", method = RequestMethod.GET)
+    @GetMapping(value= "/publico/v1/livro/{id}")
     public Livro buscaPorId(@PathVariable(value="id") Long id) {
         Livro livro = new Livro();
 
         livro.setId(id);
-        livro.setAutor("José Autor de teste");
-        livro.setTitulo("Um Livro de exemplo");
-        livro.setPrefacio("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam lobortis eros nec mi tincidunt dignissim. In hac habitasse platea dictumst. Ut condimentum ornare ante quis lobortis. Vivamus blandit gravida tincidunt. Praesent maximus nisl dolor, et cursus neque euismod ut. Ut in sapien et massa tincidunt lacinia. Phasellus tincidunt semper ligula ut porta. Duis laoreet feugiat ipsum nec euismod. In facilisis ante vel massa lobortis congue. Maecenas egestas magna a eros feugiat, quis laoreet tortor viverra. Aenean at lobortis leo. Sed ultrices odio sed orci interdum eleifend. Nullam iaculis suscipit eros quis ullamcorper. Ut nec pulvinar tellus. Sed imperdiet sapien at semper vehicula.");
+        livro.setAutor("José Autor de teste " + id);
+        livro.setTitulo("Um Livro de exemplo com o id " + id);
+        livro.setPrefacio("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam lobortis eros nec mi tincidunt dignissim.");
         
 		return livro;
+    }
+    
+    @PostMapping(value= "/publico/v1/livro/{isbn}/comentario")
+    public String postarComentario(@PathVariable(value="isbn") String isbn, @RequestBody Comentario comentario) {
+    	
+    	return "Comentário registrado.";
     }
 }
