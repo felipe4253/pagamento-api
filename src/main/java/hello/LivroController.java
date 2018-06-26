f1f093b4122a160cbc34af4340a169f30616d7ea
Felipe Silva <felipess@ciandt.com>
2018-06-26 19:21:27 -0300

    primeiro commimt

--- src/main/java/hello/LivroController.java
+++ src/main/java/hello/LivroController.java
@@ -3,18 +3,21 @@ package hello;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class LivroController {
 
-    @GetMapping(value= "/publico/v1/livro/{id}")
-    public Livro buscaPorId(@PathVariable(value="id") Long id) {
+    @GetMapping(value= "/publico/v1/livro/{id}", produces="application/json")
+    @ResponseBody
+    public ResponseEntity buscaPorId(@PathVariable(value="id") Long id) {
         Livro livro = new Livro();
 
         livro.setId(id);
@@ -22,17 +25,17 @@ public class LivroController {
         livro.setTitulo("Um Livro de exemplo com o id " + id);
         livro.setPrefacio("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam lobortis eros nec mi tincidunt dignissim.");
         
-		return livro;
+		return ResponseEntity.ok(livro);
     }
     
-    @PostMapping(value= "/publico/v1/livro/{isbn}/comentario")
-    public String postarComentario(@PathVariable(value="isbn") String isbn, @RequestBody Comentario comentario) {
+    @PostMapping(value= "/publico/v1/livro/{isbn}/comentario", consumes="application/json", produces="application/json")
+    public ResponseEntity postarComentario(@PathVariable(value="isbn") String isbn, @RequestBody Comentario comentario) {
     	
-    	return "Comentário registrado.";
+    	return ResponseEntity.ok("");
     }
     
     @GetMapping(value= "/publico/v1/livro")
-    public List<Livro> buscaPorFiltros(
+    public ResponseEntity buscaPorFiltros(
     		@RequestParam(value="isbn") String isbn, 
     		@RequestParam(value="autor") String autor) {
     	
@@ -49,6 +52,6 @@ public class LivroController {
     		
     	}
         
-		return livrosEncontrados;
+		return ResponseEntity.ok(livrosEncontrados);
     }
 }
